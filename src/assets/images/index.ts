3ea5374cdd9c9ac2d951440d5c5fdaea6c00e1ab
Nabeel Anjum <timedilation2012@gmail.com>
2024-06-16 02:32:03 +0500

    make: image carousel

---
+++ src/assets/images/index.ts
@@ -0,0 +1,10 @@
+const images = {
+  cyp1: require('./cyp1.jpg'),
+  cyp2: require('./cyp2.jpg'),
+  cyp3: require('./cyp3.jpg'),
+  cyp4: require('./cyp4.jpg'),
+  cyp5: require('./cyp5.jpg'),
+  cyp6: require('./cyp6.jpg'),
+}
+
+export default images;
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Image, StyleSheet, View } from 'react-native';
+import Carousel from 'react-native-reanimated-carousel';
 import useIpData from '../hooks/useIpData';
 import LoadingButton from '../components/shared/LoadingButton';
 import AppTextInput from '../components/shared/AppTextInput';
 import colors from '../common/colors';
 import IpInfoCard from '../components/Home/IpInfoCard';
+import { SCREEN_WIDTH } from '../common/constants';
+import images from '../assets/images';
 
 const Home = () => {
 
@@ -18,23 +21,39 @@ const Home = () => {
 
   return (
     <View style={styles.container}>
-      <AppTextInput
-        placeholder='Enter IP Address'
-        value={ip}
-        onChangeText={(updateIp)}
-      />
-      <LoadingButton
-        label='Get IP Details'
-        disabled={!ip}
-        style={{ marginTop: 15 }}
-        onPress={getIpData}
-        isLoading={isLoading}
-      />
-      <View style={styles.infoContainer}>
-        <IpInfoCard title='IP Address' value={ipData?.ip} />
-        <IpInfoCard title='Location' value={`${ipData?.city}, ${ipData?.country}`} />
-        <IpInfoCard title='ISP Provider' value={ipData?.connection?.isp} />
+      <View style={{ padding: 15 }}>
+        <AppTextInput
+          placeholder='Enter IP Address'
+          value={ip}
+          onChangeText={(updateIp)}
+          returnKeyType='done'
+        />
+        <LoadingButton
+          label='Get IP Details'
+          disabled={!ip}
+          style={{ marginTop: 15 }}
+          onPress={getIpData}
+          isLoading={isLoading}
+        />
+        <View style={styles.infoContainer}>
+          <IpInfoCard title='IP Address' value={ipData?.ip} />
+          <IpInfoCard title='Location' value={`${ipData?.city}, ${ipData?.country}`} />
+          <IpInfoCard title='ISP Provider' value={ipData?.connection?.isp} />
+        </View>
       </View>
+
+      <Carousel
+        mode='parallax'
+        width={SCREEN_WIDTH}
+        height={SCREEN_WIDTH / 1.6}
+        data={Object.values(images)}
+        renderItem={({ item }) => (
+          <View style={{ flex: 1 }}>
+            <Image style={styles.carouselImage} source={item} />
+          </View>
+        )}
+      />
+
     </View>
   );
 }
@@ -45,9 +64,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.white,
-    padding: 15,
   },
   infoContainer: {
-    // flexDirection: 'row',
+  },
+  carouselImage: {
+    width: '97%',
+    height: '100%',
+    borderRadius: 10,
   }
 });
